3deab1d821389592b0839a83077a5859427cee5d
❍⌇─➭⌗尺ℯγℯᔆ: ๑ ˚ ͙۪۪̥ ⌨ᵒᶠᶜ <145686835+Rf-reyes-5345@users.noreply.github.com>
2024-08-24 22:42:26 -0400

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -228,7 +228,7 @@ let menu = `${lenguajeGB['smsConfi2']()} *${user.genero === 0 ? '👤' : user.ge
 ┃💎 _${lenguajeGB['smsTex20']()}_ 💎
 ┃➺ _${usedPrefix}ownermenu_
 ┃
-┃✅ *_CUENTAS OFICIALES GATABOT_* ✅
+┃✅ *_CUENTAS OFICIALES KNIGHTS-OF-THE-ZODIAC-MD_* ✅
 ┃➺ _${usedPrefix}cuentasgatabot | cuentasgb_
 ┃
 *╰━━━━━━━━━━━━━⬣*`.trim()
